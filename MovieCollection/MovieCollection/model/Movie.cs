c8a35e0299f18c0b74ac2658e176983b93423648
red-to-blue-admin <marian.mutu@red-to-blue.com>
2024-08-02 12:49:14 +0300

    no message

---
+++ MovieCollection/MovieCollection/model/Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieCollection.model
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+
+        public Movie(string id, string name, int year, int duration, string description)
+        {
+            Id = id;
+            Name = name;
+            Year = year;
+            Duration = duration;
+            Description = description;
+        }
+
+
+        public override string ToString()
+        {
+            return $"id: {Id}, name: {Name}, year: {Year}, duration: {Duration}, description: {Description}";
+        }
+    }
+}
