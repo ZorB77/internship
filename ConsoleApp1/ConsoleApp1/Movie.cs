268b9498f486414ec2dfdf44aa0aba3fa5c47ce1
DESKTOP-MR90OCN\user <malina.bucur@red-to-blue.com>
2024-08-02 14:18:19 +0300

    create movie class

---
+++ ConsoleApp1/ConsoleApp1/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Title: {title}";
+        }
+    }
+}
