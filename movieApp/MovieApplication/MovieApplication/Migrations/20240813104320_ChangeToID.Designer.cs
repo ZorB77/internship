c64b0914899b24cf9260a78af86a0b74ba1207c0
DESKTOP-MR90OCN\user <malina.bucur@red-to-blue.com>
2024-08-13 13:44:15 +0300

    add migration for changing the ID name for properties

--- movieApp/MovieApplication/MovieApplication/Migrations/20240813104320_ChangeToID.Designer.cs
+++ movieApp/MovieApplication/MovieApplication/Migrations/20240813104320_ChangeToID.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieApp;
 
@@ -11,9 +12,11 @@ using MovieApp;
 namespace MovieApplication.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20240813104320_ChangeToID")]
+    partial class ChangeToID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ movieApp/MovieApplication/MovieApplication/Migrations/20240813104320_ChangeToID.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeToID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StudioID",
+                table: "Studios",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleID",
+                table: "Roles",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "ReviewID",
+                table: "Reviews",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonID",
+                table: "Persons",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieStudioID",
+                table: "MovieStudios",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieID",
+                table: "Movies",
+                newName: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Studios",
+                newName: "StudioID");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Roles",
+                newName: "RoleID");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Reviews",
+                newName: "ReviewID");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Persons",
+                newName: "PersonID");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "MovieStudios",
+                newName: "MovieStudioID");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Movies",
+                newName: "MovieID");
+        }
+    }
+}
