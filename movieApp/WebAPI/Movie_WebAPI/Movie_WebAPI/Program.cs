9841a0403b9636b027a5ab2c2cc6d369439fcd29
DESKTOP-MR90OCN\user <malina.bucur@red-to-blue.com>
2024-08-14 13:03:17 +0300

    program.cs

--- movieApp/WebAPI/Movie_WebAPI/Movie_WebAPI/Program.cs
+++ movieApp/WebAPI/Movie_WebAPI/Movie_WebAPI/Program.cs
@@ -1,15 +1,26 @@
+using MovieApp;
+using MovieApp.Services;
+using MovieApplication.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<MovieContext>();
+builder.Services.AddScoped<MovieService>();
+builder.Services.AddScoped<PersonService>();
+builder.Services.AddScoped<ReviewService>();
+builder.Services.AddScoped<RoleService>();
+builder.Services.AddScoped<StudioService>();
+builder.Services.AddScoped<MovieStudioService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// SWAGGER
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
