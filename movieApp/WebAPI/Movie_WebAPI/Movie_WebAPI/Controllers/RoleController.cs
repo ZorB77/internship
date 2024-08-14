58b0b11ba37e23167053efa5ba4824077392e797
DESKTOP-MR90OCN\user <malina.bucur@red-to-blue.com>
2024-08-14 12:06:35 +0300

    create role controller

---
+++ movieApp/WebAPI/Movie_WebAPI/Movie_WebAPI/Controllers/RoleController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models;
+using MovieApplication.Services;
+
+namespace Movie_WebAPI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleService _roleService;
+        public RoleController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("api/addRole")]
+        [HttpPost]
+        public bool AddRole(int movieId, int personId, string name)
+        {
+            return _roleService.AddRole(movieId, personId, name);
+        }
+
+        [Route("api/getRoles")]
+        [HttpGet]
+        public List<Role> GetRoles()
+        {
+            return _roleService.GetAllRoles();
+        }
+
+        [Route("api/getRoleById/ID={id}")]
+        [HttpGet]
+        public Role GetRoleById(int id)
+        {
+            return _roleService.GetRoleById(id);
+        }
+
+        [Route("api/deleteRole/ID={id}")]
+        [HttpDelete]
+        public bool DeleteRole(int id)
+        {
+            return _roleService.DeleteRole(id);
+        }
+
+        [Route("api/updateRole/ID={id}")]
+        [HttpPut]
+        public bool UpdateRole(int id, int movieId, int personId, string name)
+        {
+            return _roleService.UpdateRole(id, movieId, personId, name);
+        }
+    }
+}
