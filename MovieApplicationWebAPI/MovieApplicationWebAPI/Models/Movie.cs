8e14a294eb24f8ba00d920fbd60bb45d2dc6aaf1
Anca Alexia Nistor <91622899+915-Nistor-Anca@users.noreply.github.com>
2024-08-19 11:22:06 +0300

    Add files via upload

---
+++ MovieApplicationWebAPI/MovieApplicationWebAPI/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApplicationWebAPI.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "The name of the movie is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The release date of the movie is required.")]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "The description of the movie is required.")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "The genre of the movie is required.")]
+        public string Genre { get; set; }
+    }
+}
---
+++ MovieApplicationWebAPI/MovieApplicationWebAPI/Models/Studio.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApplicationWebAPI.Models
+{
+    public class Studio
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "The name of the studio is required.")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public int EstablishmentYear { get; set; }
+        [Required(ErrorMessage = "The location of the studio is required.")]
+        public string Location { get; set; }
+    }
+}
